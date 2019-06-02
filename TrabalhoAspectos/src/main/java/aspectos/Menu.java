3d84c981494781938ad053b1012d6684c6f1ab64
pedrocotta98 <37852363+pedrocotta98@users.noreply.github.com>
2019-06-01 22:42:34 -0300

    main, junto com a criacao do arquivo

---
+++ TrabalhoAspectos/src/main/java/aspectos/Menu.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package aspectos;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Scanner;
+import java.util.*;
+
+
+/**
+ *
+ * @author pedro
+ */
+public class Menu {
+    
+    
+    
+    
+    
+    public static void main(String[] args) throws IOException 
+    {
+        
+        System.out.println("TESTE");
+        Scanner teclado=new Scanner(System.in);
+        FileWriter arquivo=new FileWriter("arquivo11.txt");
+        PrintWriter escreve=new PrintWriter(arquivo);
+        
+        escreve.println("DADOS DENTRO DO TXT");
+        escreve.println(99);
+        escreve.println("32323232");
+        
+        escreve.close();
+    }
+}
